5516e84780f81e70f587b0cddfab0f251caa2585
tjel <tomek.jelinski@gmail.com>
2016-10-24 10:44:43 +0200

    Correct integer to int.

--- circle.cpp
+++ circle.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main( ) 
 { 
     float PI = 3.14;                // variables can be initialized during declaration 
-    integer rad; 
+    int rad; 
     cout<< "Enter the radius"; 
     cin>>rad; 
     cout<< "Area of the circle is: "<< PI * rad * rad; 
