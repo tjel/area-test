67baf73abff60a980115391cf25b485868c8b10b
tjel <tomek.jelinski@gmail.com>
2016-11-21 10:47:17 +0100

    Integrate triangle-tet.

--- circle.cpp
+++ circle.cpp
@@ -9,6 +9,21 @@ int main( )
     cout<< "Enter the radius in m: "; 
     cin>>rad; 
     cout<< "Area of the circle is (in m2): "<< PI * rad * rad<<endl; 
+
+    double a; 
+    double b;
+    double c;
+    cout<< "Enter a: "; 
+    cin>>a;
+    cout<< "Enter b: "; 
+    cin>>b;
+    cout<< "Enter c: "; 
+    cin>>c;
+ 
+    cout<< "Perimeter of the triangle is: "<< a+b+c;
+
+
+
     return 0;
 }  
 
